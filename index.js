5dc5926e9a4a1fe6915c5701faef4b01182d137d
rangermauve <RangerMauve@hotmail.com>
2015-03-03 18:40:47 -0500

    Fixed rendering not being relative to parent

--- index.js
+++ index.js
@@ -18,12 +18,24 @@ function render_boxes(boxes) {
 
 function render_box(depth, box) {
 	var label = "\t<!-- Depth:" + depth + " -->";
+	var indent = make_tabs(depth);
+	var color = make_color(depth);
+	var parent = '<rect fill="' + color + '" x="' + box.left + '" y="' + box.top + '" width="' + box.width + '" height="' + box.height + '"/>';
+	return "\n" + indent + parent + label + render_children(depth, box);
+}
+
+function render_children(depth, box) {
+	var children = box.children;
+	if (!children || !children.length) return "";
+	var indent = make_tabs(depth);
+	var rendered = children.map(par(render_box, depth + 1)).join("");
+	return "\n" + indent + '<g transform="translate(' + box.left + ',' + box.top + ')">' + rendered + "\n" + indent + "</g>";
+}
+
+function make_color(depth) {
 	var color_level = ((depth * 16 + 32).toString(16));
 	var color = "#" + color_level + color_level + color_level;
-	var indent = make_tabs(depth);
-	var parent = '<rect fill="' + color + '" x="' + box.left + '" y="' + box.top + '" width="' + box.width + '" height="' + box.height + '"/>';
-	var children = (box.children || []).map(par(render_box, depth + 1)).join("");
-	return "\n" + indent + parent + label + children;
+	return color;
 }
 
 function make_tabs(depth) {
